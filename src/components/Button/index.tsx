e3ba49b149748fa55ca849e8dd12b70175269d54
Felipe Borges <interpretadesign@gmail.com>
2023-06-07 13:39:39 -0300

    react library

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export interface IButton extends React.HTMLProps<HTMLButtonElement> {}
+
+const Button: React.FC<IButton> = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>
+}
+
+export default Button
