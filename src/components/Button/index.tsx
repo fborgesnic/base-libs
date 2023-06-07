5ae6c1f28ba522ab077597a469666c76a1a86833
Felipe Borges <interpretadesign@gmail.com>
2023-06-07 13:56:41 -0300

    Button component added

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export interface IButton extends React.HTMLProps<HTMLButtonElement> {}
 
 const Button: React.FC<IButton> = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>
+  return <button style={{width: "200px", backgroundColor: "red"}} onClick={onClick}>{children}</button>
 }
 
 export default Button
